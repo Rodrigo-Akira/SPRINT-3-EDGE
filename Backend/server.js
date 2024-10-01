bb6042a340bd7334f8d213c66c4b35165610314b
Rodrigo Akira Hirota Mori <161664760+Rodrigo-Akira@users.noreply.github.com>
2024-09-30 22:47:51 -0300

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -4,21 +4,20 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 3000;
 
-// Middleware para ler dados JSON
 app.use(bodyParser.json()); 
 
-// Endpoint para receber os dados do ESP32
+
 app.post('/dados-carro', (req, res) => {
   const { velocidade, desgastePneus, tempoPitStop } = req.body;
   
-  // Aqui você pode armazenar esses dados, processá-los, etc.
+
   console.log(`Recebido: Velocidade: ${velocidade}, Desgaste dos Pneus: ${desgastePneus}, Tempo de Pit Stop: ${tempoPitStop}`);
   
-  // Responde ao ESP32
+
   res.status(200).send('Dados recebidos com sucesso');
 });
 
-// Inicia o servidor na porta especificada
+
 app.listen(PORT, () => {
   console.log(`Servidor rodando na porta ${PORT}`);
 });
